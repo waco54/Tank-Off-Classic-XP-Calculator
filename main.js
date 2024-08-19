3a02bbea4fe7a0eeeee6325896cb074968fa18cb
Mk <matveyk201015@gmail.com>
2024-08-19 14:17:13 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+function calculate() {
+  let xpNeeded = parseFloat(document.querySelector("#xpNeeded").value);
+  let xpPerGame = parseFloat(document.querySelector("#xpPerGame").value);
+  let grindText = document.querySelector("body > h2:nth-child(21) > p");
+  
+  let grindTime;
+
+  const gameTime = 590 / 60; // 590 seconds divided by 60 seconds = minutes
+  let numberOfGames = xpNeeded / xpPerGame;
+  let timeMinutes = numberOfGames * gameTime;
+
+  // check if timeMinutes is NaN
+  if (isNaN(timeMinutes) || timeMinutes < 0) {
+    grindTime = "???";
+  } else if (timeMinutes > 60) {
+    let timeHours = timeMinutes / 60;
+    let hours = Math.floor(timeHours);
+    let minutes = Math.floor(timeMinutes % 60); // calculate remaining minutes
+    
+    let hourLabel = hours === 1 ? "hour" : "hours";
+    let minuteLabel = minutes === 1 ? "minute" : "minutes";
+    grindTime = hours + " " + hourLabel + " and " + minutes + " " + minuteLabel;
+  } else {
+    let minuteLabel = Math.round(timeMinutes) === 1 ? "minute" : "minutes";
+    grindTime = Math.round(timeMinutes) + " " + minuteLabel;
+  }
+
+  grindText.innerText = grindTime;
+}
